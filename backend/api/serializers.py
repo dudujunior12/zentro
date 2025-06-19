2b799fd6146d6e43c13100f887302a1262fc43c2
dudujunior12 <dudu12310@hotmail.com>
2025-06-19 16:39:38 -0300

    fix: serializer assigned_to field

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Categories, Tickets
+from django.contrib.auth.models import User
 
 class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,7 +8,12 @@ class CategoriesSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TicketsSerializer(serializers.ModelSerializer):
+    assigned_to = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        required=False,      # não obrigatório na entrada
+        allow_null=True      # permite valor null
+    )
     class Meta:
         model = Tickets
         fields = '__all__'
-        read_only_fields = ('created_by', 'assigned_to')
+        read_only_fields = ('created_by',)
